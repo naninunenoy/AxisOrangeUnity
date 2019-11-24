dd6b26b61d8d7f2849ec51ada1711087a98d4295
nakano yosuke <naninuneno0411@gmail.com>
2019-11-25 00:20:43 +0900

    fix unity quaternion

--- Assets/AxisOrangeUnity/AxisOrangeUnityData.cs
+++ Assets/AxisOrangeUnity/AxisOrangeUnityData.cs
@@ -13,7 +13,7 @@ namespace AxisOrange {
             timestamp = raw.timestamp;
             acc = new Vector3(raw.acc[0], raw.acc[2], raw.acc[1]);
             gyro = new Vector3(raw.gyro[0], raw.gyro[2], raw.gyro[1]);
-            quaternion = new Quaternion(raw.quaternion[0], raw.quaternion[2], raw.quaternion[1], -raw.quaternion[0]);
+            quaternion = new Quaternion(raw.quaternion[1], raw.quaternion[3], raw.quaternion[2], -raw.quaternion[0]).normalized;
         }
     }
 }
